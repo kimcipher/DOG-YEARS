b535f7ad3b8f8672c9c22b22dc65eea12ae619cf
kimcipher <kimutaij192@gmail.com>
2023-01-11 12:48:53 +0300

    first commit

---
+++ Dogyears.js
@@ -0,0 +1,23 @@
+var myAge = 24;
+// declares my age to 24
+let earlyYears = 2;
+// declared variable named early years
+earlyYears = 2 * 10.5;
+laterYears = myAge - 2;
+// age variable subtracting 2
+laterYears *= 4;
+// multiplication of dog years by 4
+console.log(earlyYears);
+console.log(laterYears);
+
+// addition of laterYears and earlyYears
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+
+myName = 'JAFFER'.toLowerCase();
+// returns name to lowercase
+
+console.log(`My name is ${myName}. I am ${earlyYears} years old in human years which is ${laterYears} years old in dog years.`);
+// concatenates the string
+
+
